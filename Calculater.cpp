961671cb758e3a0f5eb533521ecb2227de4c7602
Ccpding <119514319+Ccpding@users.noreply.github.com>
2022-12-01 21:01:41 +0530

    Calculoter.cpp
    I try to make a calculator using c++

---
+++ Calculater.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int99
+  9f;
+  int s;
+  string op;
+
+  cout<<"=====WELCOME===="<<endl;
+  cout<<endl;
+  
+  cout<<" Enter Frist Numder: ";
+  cin>>f;
+  cout<<" Enter Second Numder: ";
+  cin>>s;
+
+  cout<<endl;
+  cout<<" For ADD 'add'"<<endl;
+  cout<<endl;
+  cout<<" FOR Mines 'sub'"<<endl;
+  cout<<endl;
+  cout<<" FOR Multeplation 'mul'"<<endl;
+  cout<<endl;
+  cout<<" FOR Divied 'div'"<<endl;
+  cout<<endl;
+  cout<<" Enter Opration :"<<endl;
+  cout<<" ";
+  cin>>op;
+
+  if(op == "add"){
+    cout<<" ANSWER:"<<endl;
+    cout<<" "<<(f+s)<<endl;
+  }else if(op == "sub"){
+    cout<<"ANSWER"<<endl;
+    cout<<" "<<(f-s)<<endl;
+  }else if(op == "mul"){
+    cout<<"ANSWER"<<endl;
+    cout<<" "<<(f*s)<<endl;
+  }else if (op == "div"){
+    cout<<"ANSWER"<<endl;
+    cout<<" "<<(f/s)<<endl;
+  }else{
+    cout<<"sorry"<<endl;
+  }
+
+  return 0;
+}
